e8a92f5ac0b02a3f2e5abb50caa7a943563a4d76
tybrianna <tybriannahall@gmail.com>
2026-06-02 13:10:36 -0400

    add schema to compare passwords to make sure they are the same

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -29,3 +29,12 @@ userSchema.pre("save", async function(next){
 
   next();
 });
+
+userSchema.methods.matchPassword =
+async function(password){
+
+  return await bcrypt.compare(
+      password,
+      this.password
+  );
+};
