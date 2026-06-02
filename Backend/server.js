2fdd6d720eb741cd652599bf5c08835d9bde587d
tybrianna <tybriannahall@gmail.com>
2026-06-02 12:40:43 -0400

    added readme file, starter code to server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,18 @@
-const express = require('express');
+// Depencencies
+require("dotenv").config();
+const express = require("express");
+// const movieRoutes = require("./routes/movieRoutes");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Route
+// app.use("/api", movieRoutes);
+
+// Port
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
