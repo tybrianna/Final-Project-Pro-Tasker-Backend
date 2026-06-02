f0dacda294d7c213e6dad3665207c1390a9154e8
tybrianna <tybriannahall@gmail.com>
2026-06-02 15:28:13 -0400

    included a way to register user in authController file

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -10,3 +10,31 @@ const generateToken = (id) => {
     }
   );
 };
+
+const registerUser = async (
+  req,
+  res
+) => {
+  const { name, email, password } =
+    req.body;
+
+  const exists =
+    await User.findOne({ email });
+
+  if (exists) {
+    return res
+      .status(400)
+      .json({ message: "User exists" });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  res.status(201).json({
+    _id: user._id,
+    token: generateToken(user._id),
+  });
+};
