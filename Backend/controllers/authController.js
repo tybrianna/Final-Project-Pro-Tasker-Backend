566a1e4d09a9e75a6161ec431f590f90fce8a073
tybrianna <tybriannahall@gmail.com>
2026-06-02 15:28:52 -0400

    included a way to login user in authController file

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -38,3 +38,33 @@ const registerUser = async (
     token: generateToken(user._id),
   });
 };
+
+const loginUser = async (
+  req,
+  res
+) => {
+  const { email, password } =
+    req.body;
+
+  const user =
+    await User.findOne({ email });
+
+  if (
+    user &&
+    (await user.matchPassword(
+      password
+    ))
+  ) {
+    res.json({
+      _id: user._id,
+      token: generateToken(user._id),
+    });
+  } else {
+    res
+      .status(401)
+      .json({
+        message:
+          "Invalid credentials",
+      });
+  }
+};
